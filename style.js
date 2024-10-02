b95568b634f8a0fee7cf701a160d6804d24b4404
Serhii-Simakov <sergejsimakov683@gmail.com>
2024-10-02 09:38:02 +0200

    First load

---
+++ style.js
@@ -0,0 +1,19 @@
+const burger = document.querySelector('.burger');
+const headerNav = document.querySelector('.nav');
+const noScroll = document.querySelector('.body');
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  headerNav.classList.toggle('active');
+  noScroll.classList.toggle('lock');
+});
+
+
+// Получаем все элементы "button"
+let elements = document.querySelector(".button");
+
+// Добавляем обработчик события "click" к каждому элементу
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", function() {
+    alert("Кнопка нажата!");
+  });
+}
